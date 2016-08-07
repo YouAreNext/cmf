e9e30069d6a66fbf92316b9aee10e3998e7de92f
YouAreNext <wolfikkam@gmail.com>
2016-08-07 11:22:13 +0500

    pewpew

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=cmf',
+    'username' => 'root',
+    'password' => '135462crm',
+    'charset' => 'utf8',
+];
